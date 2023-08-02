983f9e12c5be692533915e473a52e476fcf88367
Carlos-Siebra <carlosarmandosiebra@gmail.com>
2023-08-02 00:37:18 -0300

    Lógica de Xeque

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Erro de taboleiro: é necessário no mínimo uma linha e uma colua");
+			throw new BoardException("Erro de taboleiro: é necessário no mínimo uma linha e uma coluna");
 		}
 		this.rows = rows;
 		this.columns = columns;
